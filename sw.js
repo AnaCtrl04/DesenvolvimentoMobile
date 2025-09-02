dcfb6dd3bfbf4f384b803eb407605980d098f539
AnaCtrl04 <carol.anapm2004@gmail.com>
2025-09-01 21:24:54 -0300

    first commit

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "livros2025-cache-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/img/livro1.jpg",
+  "/img/livro2.jpeg",
+  "/img/livro3.jpeg",
+  "/img/livro4.jpg"
+];
+
+// Instala o service worker e faz cache dos arquivos
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Ativa e remove caches antigos
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Intercepta requisições e responde do cache
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
